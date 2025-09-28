c07f88e98d2fd5009998b9ce5da2f05eb83f9e32
Zachariah Mogambi <166056302+Z-Mogambi@users.noreply.github.com>
2025-09-29 01:06:16 +0300

    Rename oldest_pet.sql to pet-queries/oldest_pet.sql

--- oldest_pet.sql
+++
@@ -1,2 +0,0 @@
--- finding the name of the oldest pet
-SELECT name FROM animals AS oldest_pet ORDER BY age DESC lIMIT 1;
---
+++ pet-queries/oldest_pet.sql
@@ -0,0 +1,2 @@
+-- finding the name of the oldest pet
+SELECT name FROM animals AS oldest_pet ORDER BY age DESC lIMIT 1;
